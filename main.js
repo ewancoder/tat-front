f06f8210b77543909197de2a2f498c676333dd5c
Ivan Zyranau <ewancoder@gmail.com>
2024-01-18 10:27:37 +0300

    Add google authentication

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
+const auth = {};
+
 (async function main() {
 })();
+
+window.signin = function(response) {
+    auth.token = response.credential;
+    document.getElementById("authentication").style.display = "none";
+};
