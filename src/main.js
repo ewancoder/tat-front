81e7c8fb19f9c2870e7aaf6acfea031cdb2fc581
Ivan Zyranau <ewancoder@gmail.com>
2024-01-27 21:07:09 +0300

    Refactor sessions into a separate module

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { initializeTypingState } from './typing.js';
 import { createReplay } from './replay.js';
 import { notifier } from './notifier.js';
-import { config } from './config.js';
+import { initializeSessions } from './typing-sessions.js';
 
 // Authentication token is saved here after authenticating.
 const auth = {};
@@ -12,128 +12,16 @@ const inputAreaElement = document.getElementById('input-area');
 const inputElement = document.getElementById('input');
 const sessionsElement = document.getElementById('sessions');
 
+let sessions = undefined;
 window.onSignIn = async function(token) {
     auth.token = token;
 
     inputElement.value = '';
     inputAreaElement.classList.remove('hidden');
 
-    await reloadTypingSessions();
-    sessionsElement.classList.remove('hidden');
-}
-
-async function reloadTypingSessions() {
-    const sessions = await queryTypingSessions();
-    sessionsElement.innerHTML = '';
-
-    // TODO: Make sure new session is added to the list of sessions after a new text has been typed.
-    for (const session of sessions) {
-        const row = document.createElement('tr');
-
-        const textCol = document.createElement('td');
-        textCol.innerHTML = session.text;
-
-        const lengthCol = document.createElement('td');
-        lengthCol.innerHTML = `${session.lengthSeconds} seconds`;
-        lengthCol.classList.add('length-col');
-
-        const replayCol = document.createElement('td');
-        replayCol.innerHTML = '▶';
-        replayCol.classList.add('clickable');
-
-        const onClick = async function() {
-            const loadedSession = await queryTypingSession(session.id);
-
-            replay.replayTypingSession(loadedSession.text, loadedSession.events);
-        };
-        replayCol.onclick = onClick;
-
-        const deleteCol = document.createElement('td');
-        const rollbackCol = document.createElement('td');
-
-        deleteCol.innerHTML = '✖';
-        deleteCol.classList.add('clickable');
-
-        deleteCol.onclick = async function() {
-            await deleteTypingSession(session.id);
-            deleteCol.classList.add('hidden');
-            rollbackCol.classList.remove('hidden');
-            replayCol.onclick = undefined;
-            replayCol.classList.remove('clickable');
-            replayCol.classList.add('disabled');
-        };
-
-        rollbackCol.innerHTML = '⎌';
-        rollbackCol.classList.add('clickable');
-        rollbackCol.classList.add('hidden');
-
-        rollbackCol.onclick = async function() {
-            await rollbackDeletingTypingSession(session.id);
-            rollbackCol.classList.add('hidden');
-            deleteCol.classList.remove('hidden');
-            replayCol.onclick = onClick;
-            replayCol.classList.add('clickable');
-            replayCol.classList.remove('disabled');
-        }
-
-        row.appendChild(textCol);
-        row.appendChild(lengthCol);
-        row.appendChild(replayCol);
-        row.appendChild(deleteCol);
-        row.appendChild(rollbackCol);
-
-        sessionsElement.appendChild(row);
-    }
-}
-
-async function deleteTypingSession(id) {
-    // TODO: Add error handling.
-    await fetch(`${config.typingApiUrl}/${id}`, {
-        method: 'DELETE',
-        headers: {
-            'Authorization': `Bearer ${auth.token}`
-        }
-    });
-}
-
-async function rollbackDeletingTypingSession(id) {
-    // TODO: Add error handling.
-    await fetch(`${config.typingApiUrl}/${id}/rollback-archive`, {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${auth.token}`
-        }
-    });
-}
-
-async function queryTypingSessions() {
-    try {
-        const response = await fetch(config.typingApiUrl, {
-            headers: {
-                'Authorization': `Bearer ${auth.token}`
-            }
-        });
-
-        return await response.json();
-    }
-    catch {
-        notifier.alertError("Could not get user typing sessions");
-    }
-}
-
-async function queryTypingSession(id) {
-    try {
-        const response = await fetch(`${config.typingApiUrl}/${id}`, {
-            headers: {
-                'Authorization': `Bearer ${auth.token}`
-            }
-        });
-
-        return await response.json();
-    }
-    catch {
-        notifier.alertError("Could not load user typing session");
-    }
+    // TODO: Consider initializing on startup, and having a separate method to get the data here.
+    // Or after authentication is done, on async background callback.
+    sessions = await initializeSessions(auth, notifier, replay, sessionsElement);
 }
 
 window.submitText = async function submitText() {
@@ -150,7 +38,7 @@ window.submitText = async function submitText() {
 
     const text = await getNextText();
     inputAreaElement.classList.add('hidden');
-    sessionsElement.classList.add('hidden');
+    sessions.hide();
     typingState.prepareText(text);
 
     replay.stop();
@@ -158,36 +46,15 @@ window.submitText = async function submitText() {
     document.addEventListener('keyup', replay.processKeyUp);
 }
 
-async function uploadResults(results) {
-    try {
-        await fetch(config.typingApiUrl, {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${auth.token}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(results)
-        });
-
-        notifier.alertSuccess("Typing statistics have been saved");
-    }
-    catch {
-        notifier.alertError("Failed to upload typing statistics");
-        return;
-    }
-
-    await reloadTypingSessions();
-}
-
 function showControls() {
     inputElement.value = '';
     inputAreaElement.classList.remove('hidden');
-    sessionsElement.classList.remove('hidden');
+    sessions.show();
 }
 
 const typingState = initializeTypingState(textElement, async data => {
     if (!replay.isReplaying()) {
-        uploadResults(data); // Intentionally not awaited for faster UI experience.
+        sessions.uploadResults(data); // Intentionally not awaited for faster UI experience.
         showControls();
 
         document.removeEventListener('keydown', replay.processKeyDown);
---
+++ src/typing-sessions.js
@@ -0,0 +1,164 @@
+import { config } from './config.js';
+
+export async function initializeSessions(auth, notifier, replay, domElement) {
+    let infos = await queryTypingSessions();
+    let sessionsCache = {};
+
+    for (const info of infos) {
+        appendInfoToDom(info);
+    }
+
+    domElement.classList.remove('hidden');
+
+    async function queryTypingSessions() {
+        try {
+            const response = await fetch(config.typingApiUrl, {
+                headers: {
+                    'Authorization': `Bearer ${auth.token}`
+                }
+            });
+
+            return await response.json();
+        }
+        catch {
+            notifier.alertError("Could not get user typing sessions");
+        }
+    }
+
+    async function queryTypingSession(id) {
+        if (sessionsCache[id]) return sessionsCache[id];
+
+        try {
+            const response = await fetch(`${config.typingApiUrl}/${id}`, {
+                headers: {
+                    'Authorization': `Bearer ${auth.token}`
+                }
+            });
+
+            const session = await response.json();
+            sessionsCache[id] = session;
+            return session;
+        }
+        catch {
+            notifier.alertError("Could not load user typing session");
+        }
+    }
+
+    async function uploadResults(results) {
+        try {
+            const response = await fetch(config.typingApiUrl, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${auth.token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(results)
+            });
+
+            // TODO: Move outside of this try block.
+            const info = await response.json();
+
+            notifier.alertSuccess("Typing statistics have been saved");
+
+            appendInfoToDom(info, true);
+            return info;
+        }
+        catch {
+            notifier.alertError("Failed to upload typing statistics");
+        }
+    }
+
+    function appendInfoToDom(info, insertAsFirst) {
+        const row = document.createElement('tr');
+
+        const textCol = document.createElement('td');
+        textCol.innerHTML = info.text;
+
+        const lengthCol = document.createElement('td');
+        lengthCol.innerHTML = `${info.lengthSeconds} seconds`;
+        lengthCol.classList.add('length-col');
+
+        const replayCol = document.createElement('td');
+        replayCol.innerHTML = '▶';
+        replayCol.classList.add('clickable');
+
+        const onClick = async function() {
+            const loadedSession = await queryTypingSession(info.id);
+
+            replay.replayTypingSession(loadedSession.text, loadedSession.events);
+        };
+        replayCol.onclick = onClick;
+
+        const deleteCol = document.createElement('td');
+        const rollbackCol = document.createElement('td');
+
+        deleteCol.innerHTML = '✖';
+        deleteCol.classList.add('clickable');
+
+        deleteCol.onclick = async function() {
+            await deleteTypingSession(info.id);
+            deleteCol.classList.add('hidden');
+            rollbackCol.classList.remove('hidden');
+            replayCol.onclick = undefined;
+            replayCol.classList.remove('clickable');
+            replayCol.classList.add('disabled');
+        };
+
+        rollbackCol.innerHTML = '⎌';
+        rollbackCol.classList.add('clickable');
+        rollbackCol.classList.add('hidden');
+
+        rollbackCol.onclick = async function() {
+            await rollbackDeletingTypingSession(info.id);
+            rollbackCol.classList.add('hidden');
+            deleteCol.classList.remove('hidden');
+            replayCol.onclick = onClick;
+            replayCol.classList.add('clickable');
+            replayCol.classList.remove('disabled');
+        }
+
+        row.appendChild(textCol);
+        row.appendChild(lengthCol);
+        row.appendChild(replayCol);
+        row.appendChild(deleteCol);
+        row.appendChild(rollbackCol);
+
+        if (insertAsFirst) {
+            domElement.insertBefore(row, domElement.firstChild);
+        } else {
+            domElement.appendChild(row);
+        }
+    }
+
+    async function deleteTypingSession(id) {
+        // TODO: Add error handling.
+        await fetch(`${config.typingApiUrl}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${auth.token}`
+            }
+        });
+    }
+
+    async function rollbackDeletingTypingSession(id) {
+        // TODO: Add error handling.
+        await fetch(`${config.typingApiUrl}/${id}/rollback-archive`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${auth.token}`
+            }
+        });
+    }
+
+    return {
+        hide() {
+            domElement.classList.add('hidden');
+        },
+
+        show() {
+            domElement.classList.remove('hidden');
+        },
+
+        uploadResults: uploadResults
+    };
+}
